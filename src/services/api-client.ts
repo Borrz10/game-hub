e119c56e2b91cb439eb31dc4240f7c9cc143b925
Mohamed Ahmed Hassan <madaax.king@gmail.com>
2025-01-06 16:40:52 +0400

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,29 +1,35 @@
 import axios, { AxiosRequestConfig } from "axios";
- 
-export interface FetchResponse<T>{
-    count:number;
-    next : string | null;
-    results:T[];
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
 }
 
-const axiosInstance= axios.create({
-    baseURL:'https://api.rawg.io/api' , 
-    params:{
-        key: '7bde8211a36140048ecda356bd878173'
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "7bde8211a36140048ecda356bd878173",
+  },
+});
 
-class APIClient<T>{
-    endpoint:string
-    constructor(endpoint: string){
-        this.endpoint = endpoint
-    }
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
 
-    getAll=(config:AxiosRequestConfig)=>{
-        return axiosInstance
-        .get<FetchResponse<T>>(this.endpoint , config)
-        .then(res=>res.data);
-    }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
